e98a8371cfcee6cac3f0ed3effa4ae47ac303ed6
Alisher Nurmanov <alishercodecrafter@gmail.com>
2025-05-27 13:55:17 +0500

    Add edge case tests for CalculatePacks method and clean up test case naming

--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/alishercodecrafter/orderpackscalculator/internal/model"
@@ -101,40 +102,111 @@ func TestPacksServiceImpl_CalculatePacks(t *testing.T) {
 	service := NewPacksService(mockRepo)
 
 	testCases := []struct {
-		name          string
 		orderSize     int
 		expectedPacks map[int]int
 	}{
 		{
-			name:          "Order size 1",
 			orderSize:     1,
 			expectedPacks: map[int]int{250: 1},
 		},
 		{
-			name:          "Order size 250",
 			orderSize:     250,
 			expectedPacks: map[int]int{250: 1},
 		},
 		{
-			name:          "Order size 251",
 			orderSize:     251,
 			expectedPacks: map[int]int{500: 1},
 		},
 		{
-			name:          "Order size 501",
 			orderSize:     501,
 			expectedPacks: map[int]int{250: 1, 500: 1},
 		},
 		{
-			name:          "Order size 12001",
 			orderSize:     12001,
 			expectedPacks: map[int]int{250: 1, 2000: 1, 5000: 2},
 		},
 	}
 
 	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
+		t.Run(fmt.Sprintf("Order size %d", tc.orderSize), func(t *testing.T) {
 			result, err := service.CalculatePacks(tc.orderSize)
+			require.NoError(t, err)
+
+			require.Equal(t, tc.orderSize, result.OrderSize)
+			require.Equal(t, tc.expectedPacks, result.Packs)
+		})
+	}
+}
+
+func TestPacksServiceImpl_CalculatePacksEdgeCases(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockRepo := NewMockPacksRepository(ctrl)
+	packs := model.Packs{
+		{PackSize: 10},
+		{PackSize: 15},
+		{PackSize: 20},
+		{PackSize: 50},
+		{PackSize: 100},
+	}
+
+	// Since this will be called for each test case, we use AnyTimes()
+	mockRepo.EXPECT().GetPacks().Return(packs).AnyTimes()
+
+	service := NewPacksService(mockRepo)
+
+	testCases := []struct {
+		orderSize       int
+		expectedPacks   map[int]int
+		isErrorExpected bool
+	}{
+		{
+			orderSize:       -12,
+			isErrorExpected: true,
+		},
+		{
+			orderSize:       0,
+			isErrorExpected: true,
+		},
+		{
+			orderSize:     1,
+			expectedPacks: map[int]int{10: 1},
+		},
+		{
+			orderSize:     250,
+			expectedPacks: map[int]int{100: 2, 50: 1},
+		},
+		{
+			orderSize:     251,
+			expectedPacks: map[int]int{100: 2, 50: 1, 10: 1},
+		},
+		{
+			orderSize:     17,
+			expectedPacks: map[int]int{20: 1},
+		},
+		{
+			orderSize:     40,
+			expectedPacks: map[int]int{20: 2},
+		},
+		{
+			orderSize:     23,
+			expectedPacks: map[int]int{20: 1, 10: 1},
+		},
+		{
+			orderSize:     111,
+			expectedPacks: map[int]int{100: 1, 15: 1},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("Order size %d", tc.orderSize), func(t *testing.T) {
+			result, err := service.CalculatePacks(tc.orderSize)
+			if tc.isErrorExpected {
+				require.Error(t, err)
+
+				return
+			}
 			require.NoError(t, err)
 
 			require.Equal(t, tc.orderSize, result.OrderSize)
